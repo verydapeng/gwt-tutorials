1c5d1ab0104c94f7315323c6cf73f149c28f2dfc
dapeng <dapeng@dapengliu>
2014-02-05 15:47:31 +0800

    project setup, http://code.verydapeng.com/2014/02/gwt-maven-based-project-setup.html

---
+++ Project-Setup/src/main/java/com/verydapeng/project/setup/client/Entry.java
@@ -0,0 +1,12 @@
+package com.verydapeng.project.setup.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class Entry implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        Window.alert("hello world");
+    }
+}
